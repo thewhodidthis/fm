87b8d95fa3ead3948304a4b2ee29c7b5d82b5272
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-09-27 12:14:09 +0200

    module: cleanup, skip the starting of oscillators part

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict';
 
-var createSynth = function (audio, base, ratio, pimp) {
+var createVoice = function (audio, base, ratio, pimp) {
   if ( audio === void 0 ) audio = new AudioContext();
   if ( base === void 0 ) base = 440;
   if ( ratio === void 0 ) ratio = 1;
   if ( pimp === void 0 ) pimp = 80;
 
+  var currentTime = audio.currentTime;
   var carrier = { vco: audio.createOscillator(), vca: audio.createGain() };
 
-  carrier.vco.start();
-  carrier.vco.frequency.setValueAtTime(base, audio.currentTime);
+  carrier.vco.frequency.setValueAtTime(base, currentTime);
   carrier.vco.connect(carrier.vca);
 
   var modulator = { vco: audio.createOscillator(), vca: audio.createGain() };
 
-  modulator.vco.start();
-  modulator.vco.frequency.setValueAtTime(base * ratio, audio.currentTime);
+  modulator.vco.frequency.setValueAtTime(base * ratio, currentTime);
   modulator.vco.connect(modulator.vca);
 
-  modulator.vca.gain.setValueAtTime(pimp, audio.currentTime);
+  modulator.vca.gain.setValueAtTime(pimp, currentTime);
   modulator.vca.connect(carrier.vco.frequency);
 
   return { carrier: carrier, modulator: modulator }
 };
 
-module.exports = createSynth;
+module.exports = createVoice;
 
--- index.mjs
+++ index.mjs
@@ -1,20 +1,19 @@
-const createSynth = (audio = new AudioContext(), base = 440, ratio = 1, pimp = 80) => {
+const createVoice = (audio = new AudioContext(), base = 440, ratio = 1, pimp = 80) => {
+  const { currentTime } = audio
   const carrier = { vco: audio.createOscillator(), vca: audio.createGain() }
 
-  carrier.vco.start()
-  carrier.vco.frequency.setValueAtTime(base, audio.currentTime)
+  carrier.vco.frequency.setValueAtTime(base, currentTime)
   carrier.vco.connect(carrier.vca)
 
   const modulator = { vco: audio.createOscillator(), vca: audio.createGain() }
 
-  modulator.vco.start()
-  modulator.vco.frequency.setValueAtTime(base * ratio, audio.currentTime)
+  modulator.vco.frequency.setValueAtTime(base * ratio, currentTime)
   modulator.vco.connect(modulator.vca)
 
-  modulator.vca.gain.setValueAtTime(pimp, audio.currentTime)
+  modulator.vca.gain.setValueAtTime(pimp, currentTime)
   modulator.vca.connect(carrier.vco.frequency)
 
   return { carrier, modulator }
 }
 
-export default createSynth
+export default createVoice
