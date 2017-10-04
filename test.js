72786a3a1b58c257b46ac984117e5462d51f3078
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-04 23:56:12 +0200

    test: expand

--- test.js
+++ test.js
@@ -4,9 +4,24 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext
 
 const kpow = require('kpow')
 const test = require('tape')
+const createSynth = require('./')
 
 kpow()
 
 test('will default', (t) => {
+  const { carrier, modulator } = createSynth()
+
+  t.ok(carrier.hasOwnProperty('vco'))
+  t.ok(carrier.vco instanceof OscillatorNode)
+
+  t.ok(carrier.hasOwnProperty('vca'))
+  t.ok(carrier.vca instanceof GainNode)
+
+  t.ok(modulator.hasOwnProperty('vco'))
+  t.ok(modulator.vco instanceof OscillatorNode)
+
+  t.ok(modulator.hasOwnProperty('vca'))
+  t.ok(modulator.vca instanceof GainNode)
+
   t.end()
 })
