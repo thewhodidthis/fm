e63832f9a1d2636a1a6a02e0bfbfedfc38416e79
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-01-20 12:56:11 +0100

    module: buble cjs version after all

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const createVoice = ({ context = new AudioContext(), frequency = 440, ratio = 1, depth = 80 } = {}) => {
-  const { currentTime } = context;
-  const carrier = { vco: context.createOscillator(), vca: context.createGain() };
+var createVoice = function (ref) {
+  if ( ref === void 0 ) ref = {};
+  var context = ref.context; if ( context === void 0 ) context = new AudioContext();
+  var frequency = ref.frequency; if ( frequency === void 0 ) frequency = 440;
+  var ratio = ref.ratio; if ( ratio === void 0 ) ratio = 1;
+  var depth = ref.depth; if ( depth === void 0 ) depth = 80;
+
+  var currentTime = context.currentTime;
+  var carrier = { vco: context.createOscillator(), vca: context.createGain() };
 
   carrier.vco.frequency.setValueAtTime(frequency, currentTime);
   carrier.vco.connect(carrier.vca);
 
-  const modulator = { vco: context.createOscillator(), vca: context.createGain() };
+  var modulator = { vco: context.createOscillator(), vca: context.createGain() };
 
   modulator.vco.frequency.setValueAtTime(frequency * ratio, currentTime);
   modulator.vco.connect(modulator.vca);
@@ -15,7 +21,8 @@ const createVoice = ({ context = new AudioContext(), frequency = 440, ratio = 1,
   modulator.vca.gain.setValueAtTime(depth, currentTime);
   modulator.vca.connect(carrier.vco.frequency);
 
-  return { carrier, modulator }
+  return { carrier: carrier, modulator: modulator }
 };
 
 module.exports = createVoice;
+
