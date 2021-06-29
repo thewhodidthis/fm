d14e8e74e6d06c6092eb866d269ed33471536c1b
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-29 21:04:05 +0100

    test: fix for dprint

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import 'cutaway'
-import { assert, report } from 'tapeless'
-import createSynth from './index.mjs'
+import "cutaway"
+import { assert, report } from "tapeless"
+import createSynth from "./main.js"
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
@@ -8,21 +8,21 @@ const { ok } = assert
 const { carrier, modulator } = createSynth()
 
 ok
-  .describe('features a carrier vco', 'will default')
-  .test(Object.prototype.hasOwnProperty.call(carrier, 'vco'))
-  .describe('which is an OscillatorNode')
+  .describe("features a carrier vco", "will default")
+  .test(Object.prototype.hasOwnProperty.call(carrier, "vco"))
+  .describe("which is an OscillatorNode")
   .test(carrier.vco instanceof OscillatorNode)
-  .describe('features a carrier vca')
-  .test(Object.prototype.hasOwnProperty.call(carrier, 'vca'))
-  .describe('which is a GainNode')
+  .describe("features a carrier vca")
+  .test(Object.prototype.hasOwnProperty.call(carrier, "vca"))
+  .describe("which is a GainNode")
   .test(carrier.vca instanceof GainNode)
-  .describe('features a modulator vco')
-  .test(Object.prototype.hasOwnProperty.call(modulator, 'vco'))
-  .describe('which is an OscillatorNode')
+  .describe("features a modulator vco")
+  .test(Object.prototype.hasOwnProperty.call(modulator, "vco"))
+  .describe("which is an OscillatorNode")
   .test(modulator.vco instanceof OscillatorNode)
-  .describe('features a modulator vca')
-  .test(Object.prototype.hasOwnProperty.call(modulator, 'vca'))
-  .describe('which is a GainNode')
+  .describe("features a modulator vca")
+  .test(Object.prototype.hasOwnProperty.call(modulator, "vca"))
+  .describe("which is a GainNode")
   .test(modulator.vca instanceof GainNode)
 
 report()
